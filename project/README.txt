ad11a5357bd596e82175ad922c54bd82729bc320
zxmeng <z9meng@uwaterloo.ca>
2018-07-13 14:38:07 -0400

    final project

---
+++ project/README.txt
@@ -0,0 +1,2 @@
+gen_data.py, train.py, run_sr.py are for SRCNN
+gen_data_f.py, train_f.py, run_sr_f.py are for FSRCNN
---
+++ project/SuperResolution/train.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import caffe
+import time
+
+start_time = time.time()
+solver_path = 'examples/SRCNN/SRCNN_solver.prototxt'
+solver = caffe.SGDSolver(solver_path);
+solver.solve();
+
+print("--- %s seconds ---" % (time.time() - start_time))
